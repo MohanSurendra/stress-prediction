f47a4ce96084f130108ffc971d3df41ff147132d
unknown <surendrasurya1216@gmail.com>
2025-07-07 18:36:36 +0530

    Updated files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/app/**/*.{js,ts,jsx,tsx}",
-  "./src/pages/**/*.{js,ts,jsx,tsx}",
-  "./src/components/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {},
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
-export const plugins = [];
