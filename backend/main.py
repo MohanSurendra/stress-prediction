04a3f235e6c4a3056a8cf12b46a6a88324c76465
unknown <surendrasurya1216@gmail.com>
2025-07-07 18:57:15 +0530

    Updated files

--- backend/main.py
+++ backend/main.py
@@ -174,4 +174,5 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
